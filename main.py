b3cf73821f018f433bc9ac7484ae614cdaf5f444
sourav-ydv <sk.souravrao25@gmail.com>
2025-10-21 17:50:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,21 +59,21 @@ if selected == 'Diabetes Prediction':
     col1, col2, col3 = st.columns(3)
     
     with col1:
-    Pregnancies = st.text_input("Pregnancies")
+        Pregnancies = st.text_input("Pregnancies")
     with col2:
-    Glucose = st.text_input("Glucose Level")
+        Glucose = st.text_input("Glucose Level")
     with col3:
-    BloodPressure = st.text_input("Blood Pressure value")
+        BloodPressure = st.text_input("Blood Pressure value")
     with col1:
-    SkinThickness = st.text_input("Skin Thickness value")
+        SkinThickness = st.text_input("Skin Thickness value")
     with col2:
-    Insulin = st.text_input("Insulin Level")
+        Insulin = st.text_input("Insulin Level")
     with col3:
-    BMI = st.text_input("BMI value")
+        BMI = st.text_input("BMI value")
     with col1:
-    DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
+        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
     with col2:
-    Age = st.text_input("Age")
+        Age = st.text_input("Age")
 
     if st.button('Diabetes Test Result'):
         user_input_d = [int(Pregnancies), int(Glucose), int(BloodPressure),
@@ -285,4 +285,5 @@ if selected == "Upload Health Report":
         }
         st.session_state["redirect_to"] = "HealthBot Assistant"
         st.rerun()
+
 
