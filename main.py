2db17824e776a91026188dae7e40f3b34615237b
sourav-ydv <sk.souravrao25@gmail.com>
2025-10-21 17:43:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,13 +56,23 @@ if "redirect_to" in st.session_state and st.session_state["redirect_to"]:
 if selected == 'Diabetes Prediction':
     st.title("Diabetes Prediction using ML")
 
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
     Pregnancies = st.text_input("Pregnancies")
+    with col2:
     Glucose = st.text_input("Glucose Level")
+    with col3:
     BloodPressure = st.text_input("Blood Pressure value")
+    with col1:
     SkinThickness = st.text_input("Skin Thickness value")
+    with col2:
     Insulin = st.text_input("Insulin Level")
+    with col3:
     BMI = st.text_input("BMI value")
+    with col1:
     DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
+    with col2:
     Age = st.text_input("Age")
 
     if st.button('Diabetes Test Result'):
@@ -275,3 +285,4 @@ if selected == "Upload Health Report":
         }
         st.session_state["redirect_to"] = "HealthBot Assistant"
         st.rerun()
+
